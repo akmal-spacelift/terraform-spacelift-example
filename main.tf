ee87b764ae27bce20dafbe6c6c3fa515f154fbe3
akmal-spacelift <164946548+akmal-spacelift@users.noreply.github.com>
2024-04-03 10:17:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "spacelift_stack" "example-stack" {
   autodeploy        = false
   branch            = "main"
   description       = "simple gcp compute instance"
-  repository        = "stack_with_dependencies"
+  repository        = "akmal-spacelift/stack_for_module_usage"
   terraform_version = "0.12.27"
 }
 
